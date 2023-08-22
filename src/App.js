cb6be52497eea35b15d44bbd2170ebf63199ed77
Denis Sazonov <greenfish@list.ru>
2023-08-22 17:57:56 +0400

    change color map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { YMaps, Map } from '@pbe/react-yandex-maps';
 import './App.css';
+import {useState} from "react";
 
-function App() {
+const App = () => {
+    const [initial, setInitial] = useState({
+        center: [53.195250, 50.137810],
+        zoom: 14
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <YMaps>
+        <div className="App">
+            <Map className="grayMap" width={"100vw"} height={"100vh"} defaultState={initial} />
+        </div>
+      </YMaps>
   );
 }
 
