345da72c390847940c2f89a33f8fae88bc8aac91
Denis Sazonov <greenfish@list.ru>
2023-08-22 21:20:27 +0400

    build-1

--- src/components/BalloonComponent/index.jsx
+++
@@ -1,5 +0,0 @@
-const BalloonComponent = ({text, backgroundColor, color}) => {
-    return <div className='balloon' style={{backgroundColor: backgroundColor, color: color}}>{text}</div>
-}
-
-export default BalloonComponent;
