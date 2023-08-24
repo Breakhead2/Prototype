4d3bb6d8db2f4fed0b7b951101a85ebe0ad4ba84
Denis Sazonov <greenfish@list.ru>
2023-08-24 21:51:07 +0400

    create own custom placemark

--- src/App.js
+++ src/App.js
@@ -1,111 +1,119 @@
-import { YMaps, Map, Placemark, Polyline,  } from '@pbe/react-yandex-maps';
-import {useState} from "react";
+import { YMaps, Map, Placemark, Polyline } from "@pbe/react-yandex-maps";
+import { useState } from "react";
+
+// image
+import image1 from "./assets/img/image-1.png";
 
 // data
 import { data } from "./data/data.js";
 
 //components
-import MyPlacemark from './components/MyPlacemark/index.js';
-import AnotherPlacemark from './components/AnotherPlacemark/AnotherPlacemark.js';
+import MyPlacemark from "./components/MyPlacemark/index.js";
+import AnotherPlacemark from "./components/AnotherPlacemark/AnotherPlacemark.js";
 
 // style
 import "./App.css";
 
 const App = () => {
+  const defaultState = {
+    center: [53.19525, 50.13781],
+    zoom: 14,
+  };
 
-    const defaultState = {
-        center: [53.195250, 50.137810],
-        zoom: 14
-    };
+  // const fullPackage = 'package.full';
 
-    // const fullPackage = 'package.full';
+  const [ymaps, setYmaps] = useState(null);
 
-    const [ymaps, setYmaps] = useState(null);
-
-    const places = [
-        {
-            id: 1,
-            coord: [53.203793, 50.109937],
-            options: {
-                preset: "islands#circleIcon",
-                iconColor: '#0049ff',
-                hideIconOnBalloonOpen: false,
-            },
-            properties: {
-                balloonContentBody: "Монумент Славы",
-            }
-        },
-        {
-            id: 2,
-            coord: [53.195179, 50.103088],
-            options: {
-                preset: "islands#circleIcon",
-                iconColor: '#ff003b',
-                hideIconOnBalloonOpen: false,
-            },
-            properties: {
-                balloonContentBody: "Самарский академический театр оперы и баллета",
-            }
-        },
-        {
-            id: 3,
-            coord: [53.212712, 50.145279],
-            options: {
-                preset: "islands#circleIcon",
-                iconColor: '#A68900',
-                hideIconOnBalloonOpen: false,
-            },
-            properties: {
-                balloonContentBody: "Музей Самара космическая",
-            }
-        }
-    ];
+  const places = [
+    {
+      id: 1,
+      coord: [53.203793, 50.109937],
+      options: {
+        preset: "islands#circleIcon",
+        iconColor: "#0049ff",
+        hideIconOnBalloonOpen: false,
+      },
+      properties: {
+        balloonContentBody: "Монумент Славы",
+      },
+    },
+    {
+      id: 2,
+      coord: [53.195179, 50.103088],
+      options: {
+        preset: "islands#circleIcon",
+        iconColor: "#ff003b",
+        hideIconOnBalloonOpen: false,
+      },
+      properties: {
+        balloonContentBody: "Самарский академический театр оперы и баллета",
+      },
+    },
+    {
+      id: 3,
+      coord: [53.212712, 50.145279],
+      options: {
+        preset: "islands#circleIcon",
+        iconColor: "#A68900",
+        hideIconOnBalloonOpen: false,
+      },
+      properties: {
+        balloonContentBody: "Музей Самара космическая",
+      },
+    },
+  ];
 
   return (
-      <YMaps query={{ load: 'templateLayoutFactory', apikey: 'd1e0a31c-e1fb-4e9f-ae5c-b94400d2b79e'}} >
-        <div className="App">
-            <Map 
-                onLoad={(ymaps) => setYmaps(ymaps)}
-                width={"100vw"} 
-                height={"100vh"} 
-                defaultState={defaultState}
-                >
-                {places.map(place => <MyPlacemark key={place.id} ymaps={ymaps} data={place} />)}
-                <AnotherPlacemark ymaps={ymaps} />
+    <YMaps
+      query={{
+        load: "templateLayoutFactory",
+        apikey: "d1e0a31c-e1fb-4e9f-ae5c-b94400d2b79e",
+      }}
+    >
+      <div className="App">
+        <Map
+          onLoad={(ymaps) => setYmaps(ymaps)}
+          width={"100vw"}
+          height={"100vh"}
+          defaultState={defaultState}
+        >
+          {places.map((place) => (
+            <MyPlacemark key={place.id} ymaps={ymaps} data={place} />
+          ))}
+          <AnotherPlacemark ymaps={ymaps} image={image1} />
 
-                <Polyline
-                    geometry={[
-                        [53.212712, 50.145279],
-                        [53.211903, 50.143945],
-                        [53.210992, 50.142142],
-                        [53.210065, 50.140394],
-                        [53.209278, 50.138514],
-                        [53.207731, 50.135464],
-                        [53.209483, 50.133830],
-                        [53.212138, 50.128988],
-                        [53.212595, 50.126683],
-                        [53.212595, 50.126683],
-                        [53.212595, 50.126777],
-                        [53.212595, 50.126888],
-                        [53.212595, 50.126999],
-                        [53.212595, 50.127000],
-                        [53.212595, 50.128888],
-                        [53.212595, 50.128999],
-                        [53.212595, 50.129000],
-                    ]}
-                    options={{
-                        balloonCloseButton: false,
-                        strokeColor: "#081272",
-                        strokeWidth: 4,
-                        strokeOpacity: 0.5,
-                        strokeStyle: "shortdash",
-                    }}
-                />
-
-            </Map>
-        </div>
-      </YMaps>
+          <Polyline
+            geometry={[
+              [53.212712, 50.145279],
+              [53.211903, 50.143945],
+              [53.210992, 50.142142],
+              [53.210065, 50.140394],
+              [53.209278, 50.138514],
+              [53.207731, 50.135464],
+              [53.209483, 50.13383],
+              [53.212138, 50.128988],
+              [53.212595, 50.126683],
+              [53.212595, 50.126683],
+              [53.212595, 50.126777],
+              [53.212595, 50.126888],
+              [53.212595, 50.126999],
+              [53.212595, 50.127],
+              [53.212595, 50.128888],
+              [53.212595, 50.128999],
+              [53.212595, 50.129],
+            ]}
+            options={{
+              balloonCloseButton: false,
+              strokeColor: "#081272",
+              strokeWidth: 4,
+              strokeOpacity: 0.5,
+              strokeStyle: "shortdash",
+            }}
+          />
+        </Map>
+      </div>
+    </YMaps>
   );
-}
+};
 
 export default App;
--- src/components/AnotherPlacemark/AnotherPlacemark.js
+++ src/components/AnotherPlacemark/AnotherPlacemark.js
@@ -1,72 +1,83 @@
 import { Placemark } from "@pbe/react-yandex-maps";
 import { useState, useEffect } from "react";
 import $ from "jquery";
+import "./AnotherPlacemark.css";
 
-const AnotherPlacemark = ({ ymaps }) => {
-  
+const AnotherPlacemark = ({ ymaps, image }) => {
   const [template, setTemplate] = useState(null);
 
-  let myBalloonLayout = ymaps?.templateLayoutFactory.createClass('<div class="popover top">' +
-  '<a class="close" href="#">&times;</a>' +
-  '<div class="arrow"></div>' +
-  '<div class="popover-inner">' +
-  '$[[options.contentLayout observeSize minWidth=auto maxWidth=auto maxHeight=350]]' +
-  '</div>' +
-  '</div>', {
+  let myBalloonLayout = ymaps?.templateLayoutFactory.createClass(
+    '<div class="popup">' +
+      '<div class="arrow"></div>' +
+      '<div class="popup-inner">' +
+      "$[[options.contentLayout observeSize minWidth=auto maxWidth=auto maxHeight=350]]" +
+      "</div>" +
+      '<a class="close" href="#">&times;</a>' +
+      "</div>",
+    {
+      build: function () {
+        this.constructor.superclass.build.call(this);
 
-    build: function () {
-      this.constructor.superclass.build.call(this);
+        this._$element = $(".popup", this.getParentElement());
 
-      this._$element = $('.popover', this.getParentElement());
+        this.applyElementOffset();
 
-      this.applyElementOffset();
+        this._$element
+          .find(".close")
+          .on("click", $.proxy(this.onCloseClick, this));
+      },
 
-      this._$element.find('.close')
-          .on('click', $.proxy(this.onCloseClick, this));
-    },
+      clear: function () {
+        this._$element.find(".close").off("click");
 
-    clear: function () {
-      this._$element.find('.close')
-          .off('click');
+        this.constructor.superclass.clear.call(this);
+      },
 
-      this.constructor.superclass.clear.call(this);
-    },
-    
-    applyElementOffset: function () {
-      this._$element.css({
-        left: -(this._$element[0].offsetWidth / 2),
-        top: -(this._$element[0].offsetHeight)
-      });
-    },
+      applyElementOffset: function () {
+        this._$element.css({
+          left: -45,
+          top: -this._$element[0].offsetHeight - 40,
+          // left: -(this._$element[0].offsetWidth / 2),
+          // top: -this._$element[0].offsetHeight,
+        });
+      },
 
-    onCloseClick: function (e) {
-      e.preventDefault();
-      this.events.fire('userclose');
+      onCloseClick: function (e) {
+        e.preventDefault();
+        this.events.fire("userclose");
+      },
     }
-  });
+  );
 
   let myBalloonContentLayout = ymaps?.templateLayoutFactory.createClass(
-    '<h3 class="popover-title">$[properties.balloonHeader]</h3>' +
-        '<div class="popover-content">$[properties.balloonContent]</div>'
+    `<div class="popup-inner-content">
+      <div class="image-container"><img src="${image}" alt="" /></div>
+      <div class="content">
+        <div class="popup-title">$[properties.balloonHeader]</div>
+        <div class="popup-content">$[properties.balloonContent]</div>
+      </div>
+    </div>
+    `
   );
 
   return (
     <Placemark
       modules={["geoObject.addon.balloon"]}
       geometry={[53.215904, 50.132277]}
-      options = {{ 
+      options={{
         preset: "islands#circleIcon",
-        iconColor: '#0049ff',
+        iconColor: "#0049ff",
         closeButton: false,
-        balloonLayout:myBalloonLayout,
+        balloonLayout: myBalloonLayout,
         balloonContentLayout: myBalloonContentLayout,
         hideIconOnBalloonOpen: false,
-
       }}
-      balloon={{options:{
-        closeButton: false,
-      }}}
-      properties={{balloonContent:'Стела Ладьи', balloonHeader: 'Заголовок'}}
+      balloon={{
+        options: {
+          closeButton: false,
+        },
+      }}
+      properties={{ balloonContent: "Памятник", balloonHeader: "Стела Ладьи" }}
     />
   );
 };
